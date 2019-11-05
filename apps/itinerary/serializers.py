ed7966dbe65f165387ff3a58de0962b54af0ab0f
Peter Curet <>
2019-11-05 12:02:01 +0100

    Exposes a url for a case in the Itenerary list data

--- apps/itinerary/serializers.py
+++ apps/itinerary/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from .models import Itinerary, ItineraryItem
 
-class ItineraryItemSerializer(serializers.ModelSerializer):
+class ItineraryItemSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='case-detail',
+        lookup_field='wng_id',
+        lookup_url_kwarg='pk'
+    )
+
     class Meta:
         model = ItineraryItem
-        fields = ('wng_id', 'stadium', 'address', 'postal_code_area', 'postal_code_street')
+        fields = ('url', 'wng_id', 'stadium', 'address', 'postal_code_area', 'postal_code_street')
 
 class ItinerarySerializer(serializers.ModelSerializer):
     items = ItineraryItemSerializer(many=True, read_only=True)
