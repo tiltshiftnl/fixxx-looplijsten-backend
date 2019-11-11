5fa1f5b02faf1b5ddd2259c87fc00d2c77b8b39e
Peter Curet <>
2019-11-11 12:15:37 +0100

    updates users

--- app/api/users/models.py
+++ app/api/users/models.py
@@ -5,17 +5,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.encoding import python_2_unicode_compatible
 
 from api.users.user_manager import UserManager
 from rest_framework.authtoken.models import Token
 
 
-@python_2_unicode_compatible
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = None
-    email = models.EmailField(unique=True, blank=False)
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        error_messages={
+            'unique': "A user with that email already exists.",
+        },
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -25,6 +28,9 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+    def save(self, *args, **kwargs):
+        self.username = self.email.split('@')[0]
+        super().save(*args, **kwargs)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
