1ab00b678d033532266456908c3ce18d7478eba5
Peter Curet <>
2019-11-12 13:08:20 +0100

    Load mock case dataset from json

--- app/api/itinerary/views.py
+++ app/api/itinerary/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+import json
 # from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
@@ -37,36 +38,6 @@ class CaseViewSet(viewsets.ViewSet):
     """
 
     def retrieve(self, request, pk):
-        return JsonResponse({
-            # Data using the wng_id
-            'bwv_hotline_bevinding': {},
-            'bwv_hotline_melding': {},
-            'bwv_vakantieverhuur': {},
-            'import_adres': {},
-            # Data using the adres_id
-            'bwv_personen_hist': {},
-            'bwv_personen': {},
-            'import_stadia': {},
-            'import_wvs': {}
-        })
-
-        # TODO: For now we'll just return an empty response
-
-        # queryset = ItineraryItem.objects.all()
-        # itinerary_item = get_object_or_404(queryset, wng_id=pk)
-
-        # wng_id = itinerary_item.wng_id
-        # adres_id = itinerary_item.adres_id
-
-        # return JsonResponse({
-        #     # Data using the wng_id
-        #     'bwv_hotline_bevinding': get_data_from_id('/app/datasets/bwv_hotline_bevinding.csv', wng_id, 'wng_id'),
-        #     'bwv_hotline_melding': get_data_from_id('/app/datasets/bwv_hotline_melding.csv', wng_id, 'wng_id'),
-        #     'bwv_vakantieverhuur': get_data_from_id('/app/datasets/bwv_vakantieverhuur.csv', wng_id, 'wng_id'),
-        #     'import_adres': get_data_from_id('/app/datasets/import_adres.csv', wng_id, 'wng_id')[0],
-        #     # Data using the adres_id
-        #     'bwv_personen_hist': get_data_from_id('/app/datasets/bwv_personen_hist.csv', adres_id, 'ads_id'),
-        #     'bwv_personen': get_data_from_id('/app/datasets/bwv_personen.csv', adres_id, 'ads_id_wa'),
-        #     'import_stadia': get_data_from_id('/app/datasets/import_stadia.csv', adres_id, 'adres_id'),
-        #     'import_wvs': get_data_from_id('/app/datasets/import_wvs.csv', adres_id, '\ufeff"adres_id"')
-        # })
+        with open('/app/datasets/case.json') as json_file:
+            data = json.load(json_file)
+            return JsonResponse(data)
