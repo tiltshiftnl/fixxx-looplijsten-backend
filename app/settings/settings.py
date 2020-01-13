5388f0952010143e2ad00400c2aac3ada63acdcb
Peter Curet <>
2020-01-13 13:41:16 -0400

    fix

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = (
 
     # Third party apps
     'rest_framework',            # utilities for rest apis
+    'rest_framework.authtoken',
     'django_filters',            # for filtering rest endpoints
     'drf_yasg',                  # for generating real Swagger/OpenAPI 2.0 specifications
     'constance',
