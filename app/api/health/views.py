2f5161774c85b1459390c2cfb6ddd155c5cd39f3
Peter Curet <>
2020-01-23 14:20:26 -0400

    Tests for health connectivity

--- app/api/health/views.py
+++ app/api/health/views.py
@@ -2,12 +2,16 @@ from django.db import Error, connections
 from django.http import HttpResponse
 from django.conf import settings
 
+SUCCESS_MESSAGE_DEFAULT = 'Connectivity OK'
+ERROR_MESSAGE_DEFAULT = 'Database connectivity failed'
+SUCCESS_MESSAGE_BWV = 'BWV Connectivity OK'
+ERROR_MESSAGE_BWV = 'BWV Database connectivity failed'
 
 def health_generic(request, database_name, success_message, error_message):
     try:
-        with connections[database_name].cursor() as cursor:
-            cursor.execute('select 1')
-            assert cursor.fetchone()
+        cursor = connections[database_name].cursor()
+        cursor.execute('select 1')
+        assert cursor.fetchone()
     except Error:
         return HttpResponse(error_message, content_type='text/plain', status=500)
     else:
@@ -15,15 +19,15 @@ def health_generic(request, database_name, success_message, error_message):
 
 
 def health_default(request):
-    return health_generic(request,
-                          settings.DEFAULT_DATABASE_NAME,
-                          'Connectivity OK',
-                          'Database connectivity failed',
+    return health_generic(request=request,
+                          database_name=settings.DEFAULT_DATABASE_NAME,
+                          success_message=SUCCESS_MESSAGE_DEFAULT,
+                          error_message=ERROR_MESSAGE_DEFAULT
                           )
 
 def health_bwv(request):
-    return health_generic(request,
-                          settings.BWV_DATABASE_NAME,
-                          'BWV Connectivity OK',
-                          'BWV Database connectivity failed',
+    return health_generic(request=request,
+                          database_name=settings.BWV_DATABASE_NAME,
+                          success_message=SUCCESS_MESSAGE_BWV,
+                          error_message=ERROR_MESSAGE_BWV
                           )
