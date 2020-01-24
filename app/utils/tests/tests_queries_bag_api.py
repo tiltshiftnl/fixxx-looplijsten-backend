a1443a4438d436ea0386cfd7607a9d4dca83b1ba
Peter Curet <>
2020-01-24 11:52:01 -0400

    Bag search unit tests

--- app/utils/tests/tests_queries_bag_api.py
+++ app/utils/tests/tests_queries_bag_api.py
@@ -3,10 +3,9 @@ Tests for BAG queries
 """
 from django.test import TestCase
 from django.conf import settings
-from utils.queries_bag_api import get_bag_search_query, do_bag_search_address, do_bag_search_id
-from unittest.mock import patch
-
-# TODO: write tests for get_bag_data
+from utils.queries_bag_api import get_bag_search_query, do_bag_search_address, do_bag_search_id, get_bag_data
+from utils.queries_bag_api import do_bag_search
+from unittest.mock import patch, Mock
 
 class GetBagSearchQueryTest(TestCase):
 
@@ -110,3 +109,74 @@ class DoBagSearchIdTest(TestCase):
             settings.BAG_API_SEARCH_URL,
             params={'q': 'Foo ID'}
         )
+
+
+class GetBagDataTest(TestCase):
+    @patch('utils.queries_bag_api.do_bag_search')
+    @patch('utils.queries.get_import_adres')
+    @patch('requests.get')
+    def test_get_bag_data(self, mock_requests_get, mock_get_import_adres, mock_do_bag_search):
+        """
+        Does a GET requests using the URI retrieved from a BAG search
+        """
+
+        FOO_BAG_URI = 'http://FOO_BAG_URI.com/'
+
+        mock_do_bag_search.return_value = {
+            'results': [
+                {
+                    '_links': {
+                        'self': {
+                            'href': FOO_BAG_URI
+                        }
+                    }
+                }
+            ]
+        }
+
+        get_bag_data('FOO ID')
+
+        mock_requests_get.assert_called_with(FOO_BAG_URI)
+
+
+class DoBagSearchTest(TestCase):
+    @patch('utils.queries_bag_api.do_bag_search_address')
+    def test_do_bag_search(self, mock_do_bag_search):
+        """
+        Does a regular bag search using the address and returns the address search results
+        """
+
+        REGULAR_BAG_SEARCH_RESULT = {
+            'count': 1,
+            'results': ['Foo Result']
+        }
+        MOCK_ADDRRESS = Mock()
+
+        mock_do_bag_search.return_value = REGULAR_BAG_SEARCH_RESULT
+
+        result = do_bag_search(MOCK_ADDRRESS)
+
+        self.assertEquals(REGULAR_BAG_SEARCH_RESULT, result)
+
+    @patch('utils.queries_bag_api.do_bag_search_id')
+    @patch('utils.queries_bag_api.do_bag_search_address')
+    def test_do_bag_search_fallback(self, mock_do_bag_search, mock_do_bag_search_id):
+        """
+        Does a BAG search using an ID as fallback when the regular search fails
+        """
+        # Regular search returns 0 results
+        REGULAR_BAG_SEARCH_RESULT = {
+            'count': 0,
+            'results': []
+        }
+
+        mock_do_bag_search.return_value = REGULAR_BAG_SEARCH_RESULT
+
+        ID_BAG_SEARCH_RESULT = 'MOCK_BACK_SEARCH_ID Result'
+        mock_do_bag_search_id.return_value = ID_BAG_SEARCH_RESULT
+
+        MOCK_ADDRRESS = Mock()
+        result = do_bag_search(MOCK_ADDRRESS)
+
+        # As a result, the ID bag search results should be returned/used
+        self.assertEquals(ID_BAG_SEARCH_RESULT, result)
