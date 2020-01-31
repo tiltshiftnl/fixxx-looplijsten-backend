f59d27802dd20cb6cce4ee4f219cb0e860ad3519
Peter Curet <>
2020-01-31 11:52:14 +0100

    Removed deprecated and unused token creation

--- app/api/users/models.py
+++ app/api/users/models.py
@@ -3,13 +3,8 @@ import uuid
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from api.users.user_manager import UserManager
 from api.users.utils import generate_username
-from rest_framework.authtoken.models import Token
-
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -32,8 +27,3 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         self.username = generate_username(self.email)
         super().save(*args, **kwargs)
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
