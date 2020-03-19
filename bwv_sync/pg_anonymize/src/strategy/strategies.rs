c2b7b7a56eecda234f758723781779cf5ffa6491
Koen Wilde <koen@chillheid.nl>
2020-03-19 15:42:37 +0100

    Prevent generating impossible dates

--- bwv_sync/pg_anonymize/src/strategy/strategies.rs
+++ bwv_sync/pg_anonymize/src/strategy/strategies.rs
@@ -26,7 +26,7 @@ pub(crate) fn date() -> String {
         "{year}-{month:02}-{day:02} 00:00:00",
         year = rng.gen_range(1940, 2019),
         month = rng.gen_range(1, 12),
-        day = rng.gen_range(1, 30)
+        day = rng.gen_range(1, 28)
     )
 }
 
