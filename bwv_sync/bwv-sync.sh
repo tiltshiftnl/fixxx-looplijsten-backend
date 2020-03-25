1cb53fa4ac5196738a5055ed69a0ed5b4fce6201
Koen Wilde <koen@chillheid.nl>
2020-03-25 15:25:47 +0100

    Add table to track bwv sync

--- bwv_sync/bwv-sync.sh
+++ bwv_sync/bwv-sync.sh
@@ -14,7 +14,12 @@ src_host=swdbmams2204.basis.lan
 src_db=rve_wonen
 src_user=srvc_fixxx
 src_pw="${RVE_WONEN_PASSWORD}"
+dst_host="${BWV_DB_HOST}"
 dst_db="${BWV_DB_NAME}"
+dst_user="${BWV_DB_USER}"
+dst_pw="${BWV_DB_PASSWORD}"
+
+timestamp_start="$(TZ="Europe/Amsterdam" date "+%Y-%m-%d %H:%M:%S Europe/Amsterdam")"
 if test "${ENVIRONMENT}" = "acceptance"; then
 anonymize=true
 else
@@ -38,15 +43,25 @@ tables=(
   view_woningen,bwv_woningen
 )
 
+
+PGPASSWORD="${dst_pw}" psql -h "$dst_host" -U "$dst_user" -d "$dst_db" -c \
+  "CREATE TABLE IF NOT EXISTS sync_log (start timestamp with time zone, finished timestamp with time zone);"
+PGPASSWORD="${dst_pw}" psql -h "$dst_host" -U "$dst_user" -d "$dst_db" -c \
+  "INSERT INTO sync_log (start) VALUES ('${timestamp_start}');"
+
 export PGPASSWORD="${src_pw}"
 for src_dst in ${tables[@]}; do
   src_table=$(cut -d, -f 1 <<<"$src_dst")
   dst_table=$(cut -d, -f 2 <<<"$src_dst")
   echo "Syncing $src_table on ${src_host}:${src_db} to $dst_table" | tee "$logfile"
 
-  PGPASSWORD="${BWV_DB_PASSWORD}" psql -h "${BWV_DB_HOST}" -U "${BWV_DB_USER}" -c "TRUNCATE $dst_table" "$dst_db"
+  PGPASSWORD="${dst_pw}" psql -h "${dst_host}" -U "${dst_user}" -c "TRUNCATE $dst_table" "$dst_db"
   psql --no-align --tuples-only -h "$src_host" -d "$src_db" -U "$src_user" \
     -c "COPY (SELECT * FROM $src_table) TO STDOUT" \
     | if $anonymize; then /usr/local/bin/pg_anonymize -c /etc/pg_anonymize.conf "$dst_table"; else cat -; fi \
-    | PGPASSWORD="${BWV_DB_PASSWORD}" psql -h "${BWV_DB_HOST}" -U "${BWV_DB_USER}" -d "$dst_db" -c "COPY $dst_table FROM STDIN"
+    | PGPASSWORD="${dst_pw}" psql -h "${dst_host}" -U "${dst_user}" -d "$dst_db" -c "COPY $dst_table FROM STDIN"
 done
+
+timestamp_finished="$(TZ="Europe/Amsterdam" date "+%Y-%m-%d %H:%M:%S Europe/Amsterdam")"
+PGPASSWORD="${dst_pw}" psql -h "$dst_host" -U "$dst_user" -d "$dst_db" -c \
+  "UPDATE sync_log set finished = '${timestamp_finished}' where start = '${timestamp_start}';"
