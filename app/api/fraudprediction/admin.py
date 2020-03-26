8817751bb4dbdd6cc4900406d47fffa9b5dcc108
Peter Curet <petercuret@gmail.com>
2020-03-26 10:43:28 +0100

    Revert "Comment out module functionality while migrations on acceptance are broken."
    This reverts commit 8b209d88067067f8c76bb2bdb6b6fbc5b78f2c89.

--- app/api/fraudprediction/admin.py
+++ app/api/fraudprediction/admin.py
@@ -1,13 +1,13 @@
-# from django.contrib import admin
-# from api.fraudprediction.models import FraudPrediction
+from django.contrib import admin
+from api.fraudprediction.models import FraudPrediction
 
-# @admin.register(FraudPrediction)
-# class FraudPredictionAdmin(admin.ModelAdmin):
-#     list_display = ('case_id',)
+@admin.register(FraudPrediction)
+class FraudPredictionAdmin(admin.ModelAdmin):
+    list_display = ('case_id',)
 
-#     search_fields = ('case_id',)
+    search_fields = ('case_id',)
 
-#     fields = ('case_id', 'fraud_probability',
-#               'fraud_prediction', 'business_rules', 'shap_values', 'sync_date',)
+    fields = ('case_id', 'fraud_probability',
+              'fraud_prediction', 'business_rules', 'shap_values', 'sync_date',)
 
-#     readonly_fields = fields
+    readonly_fields = fields
