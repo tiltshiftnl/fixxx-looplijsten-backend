b6916f1da7960a9991c1bdf3babe7566e23b8593
Peter Curet <petercuret@gmail.com>
2020-04-01 11:36:00 +0200

    Signals for debugging threading kills

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,18 @@
 import os
-
+import logging
+import signal
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 
 application = get_wsgi_application()
+
+def service_shutdown(signum, frame):
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.info('Service shutdown information Signum {}'.format(str(signum)))
+    LOGGER.info('Service shutdown information Frame {}'.format(str(frame)))
+
+
+# For debugging purposes, catch sigterm and sigint signals
+signal.signal(signal.SIGTERM, service_shutdown)
+signal.signal(signal.SIGINT, service_shutdown)
