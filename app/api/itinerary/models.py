343d11bee7c6d2ee2b7728d73dabc1a731f60891
Peter Curet <petercuret@gmail.com>
2020-04-14 16:29:05 +0200

    Limit length of the note texts

--- app/api/itinerary/models.py
+++ app/api/itinerary/models.py
@@ -222,7 +222,7 @@ class Note(models.Model):
     """ A note for an Itinerary Item """
     itinerary_item = models.ForeignKey(ItineraryItem, on_delete=models.CASCADE,
                                        null=False, related_name='notes')
-    text = models.TextField(null=False, blank=False)
+    text = models.TextField(null=False, blank=False, max_length=1024)
     author = models.ForeignKey(to=User, null=True, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
