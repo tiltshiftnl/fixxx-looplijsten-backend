4100de3ff3e5b14247e9468359fcc86a5dba1306
Peter Curet <petercuret@gmail.com>
2020-04-20 14:40:17 +0200

    Add sync_date to the fraud_prediction serialized data

--- app/api/fraudprediction/serializers.py
+++ app/api/fraudprediction/serializers.py
@@ -4,4 +4,4 @@ from api.fraudprediction.models import FraudPrediction
 class FraudPredictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FraudPrediction
-        fields = ('fraud_probability', 'fraud_prediction', 'business_rules', 'shap_values')
+        fields = ('fraud_probability', 'fraud_prediction', 'business_rules', 'shap_values', 'sync_date')
