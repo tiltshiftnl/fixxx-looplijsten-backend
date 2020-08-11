688a3eeba6ab2a545879c025e71f743f41d6578e
Peter Curet <petercuret@gmail.com>
2020-08-11 10:59:43 +0200

    Add collectstatic to development entrypoint

--- app/deploy/entrypoint.development.sh
+++ app/deploy/entrypoint.development.sh
@@ -5,6 +5,9 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
+echo Collecting static files
+python manage.py collectstatic --no-input
+
 yes yes | python manage.py migrate --noinput
 
 chmod -R 777 /static
