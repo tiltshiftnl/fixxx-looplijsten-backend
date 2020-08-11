62f7eeab74091b1cb2ad1a31a86d847bc083ed0e
Peter Curet <petercuret@gmail.com>
2020-08-11 09:15:50 +0200

    Commented out admin user creation in entrypoint

--- app/deploy/entrypoint.sh
+++ app/deploy/entrypoint.sh
@@ -14,7 +14,7 @@ chmod -R 777 /static
 chmod -R 700 /fraud_prediction_cache
 
 # echo Create root user
-python manage.py shell -c "from apps.users.models import User; User.objects.create_superuser('admin@admin.com', 'admin')"
+# python manage.py shell -c "from apps.users.models import User; User.objects.create_superuser('admin@admin.com', 'admin')"
 
 # run uwsgi
 exec uwsgi --ini /app/deploy/config.ini # --py-auto-reload=1
