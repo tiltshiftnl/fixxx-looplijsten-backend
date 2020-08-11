ca8adf92e4257df2207c4855392d7666c69360b2
Peter Curet <petercuret@gmail.com>
2020-08-11 08:47:31 +0200

    Access on acceptance

--- app/deploy/entrypoint.sh
+++ app/deploy/entrypoint.sh
@@ -13,5 +13,8 @@ chmod -R 777 /static
 # modify permission so scoring files can be cached
 chmod -R 700 /fraud_prediction_cache
 
+# echo Create root user
+python manage.py shell -c "from apps.users.models import User; User.objects.create_superuser('admin@admin.com', 'admin')"
+
 # run uwsgi
 exec uwsgi --ini /app/deploy/config.ini # --py-auto-reload=1
