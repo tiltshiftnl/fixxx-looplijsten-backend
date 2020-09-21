571a603acc8466ce35a9728abc86b46493759fa5
Maurice Guikema <mguikema@gmail.com>
2020-09-21 15:05:45 +0200

    TOP-1: case search street name enabled

--- app/utils/queries.py
+++ app/utils/queries.py
@@ -45,6 +45,8 @@ def get_search_results(postal_code, street_number, suffix, street_name):
         + suffix_query
     )
 
+    print(query)
+
     args = {
         "postal_code": postal_code,
         "street_name": "%"+street_name + "%",
