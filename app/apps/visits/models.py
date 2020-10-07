9ad38bf54dbb51da4546c79ef4fe6e9a62ecf52a
Maurice Guikema <mguikema@gmail.com>
2020-10-07 21:13:15 +0200

    TOP-70: rebase conflicts resolved

--- app/apps/visits/models.py
+++ app/apps/visits/models.py
@@ -45,7 +45,6 @@ class Visit(models.Model):
     )
 
     situation = models.CharField(
-<<<<<<< HEAD
         max_length=50, choices=SITUATIONS, null=True, blank=True
     )
     observations = ArrayField(
@@ -53,12 +52,6 @@ class Visit(models.Model):
     )
     case_id = models.ForeignKey(
         Case, on_delete=models.CASCADE, related_name="case_visits"
-=======
-        max_length=50, choices=visits_const.SITUATIONS, null=True, blank=True
-    )
-    observations = ArrayField(
-        models.CharField(max_length=50, choices=visits_const.OBSERVATIONS), blank=True, null=True
->>>>>>> TOP-70: db dependent visits reversed
     )
     itinerary_item = models.ForeignKey(
         "itinerary.ItineraryItem",
