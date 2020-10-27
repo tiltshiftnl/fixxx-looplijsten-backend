fe46bf0f30e4a9104c8459a842ee94d9abe39e9c
Maurice Guikema <mguikema@gmail.com>
2020-10-27 10:10:16 +0100

    fix: docker compose deploy wait for docker db

--- app/deploy/entrypoint.development.sh
+++ app/deploy/entrypoint.development.sh
@@ -5,6 +5,12 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 set -x
 
+until PGPASSWORD=$DATABASE_PASSWORD psql -h $DATABASE_HOST -U $DATABASE_USER -c '\q'; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+echo "Postgres is up!"
+
 echo Collecting static files
 python manage.py collectstatic --no-input
 
